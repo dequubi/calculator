cd8aa022b43d37c17b5ddbd009c19e1222ecebad
Evgeny Kuvshinov <41694637+jug2505@users.noreply.github.com>
2021-10-26 09:35:37 +0300

    feat: CalcDropDown UI component realization

--- src/components/CalcSealing.vue
+++ src/components/CalcSealing.vue
@@ -1,10 +1,29 @@
 <template>
+  <p>2. Настройте фильтры</p>
+
+  <calc-drop-down
+      v-model="roadClimateZone"
+      :options="roadClimateZoneOptions"
+  />
+
 
 </template>
 
 <script>
+
+import CalcDropDown from "./UI/CalcDropDown";
 export default {
-  name: "CalcSealing"
+  name: "CalcSealing",
+  components: {CalcDropDown},
+  data() {
+    return {
+      roadClimateZone : '',
+      roadClimateZoneOptions : [
+        {value: 'zone1', name: 'zone1'},
+        {value: 'zone2', name: 'zone2'},
+      ]
+    }
+  }
 }
 </script>
 
--- src/components/UI/CalcDropDown.vue
+++ src/components/UI/CalcDropDown.vue
@@ -1,10 +1,34 @@
 <template>
-
+  <select v-model="modelValue" @change="changeOption">
+    <option disabled value="">Выберите из списка</option>
+    <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+    >
+      {{ option.name }}
+    </option>
+  </select>
 </template>
 
 <script>
 export default {
-  name: "CalcDropDown"
+  name: "CalcDropDown",
+
+  props: {
+    modelValue: {
+      type: String
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    changeOption(event) {
+      this.$emit('update:modelValue', event.target.value);
+    }
+  }
 }
 </script>
 
